375b3e1336700d68c180dc20db6b1be68cc1aae3
apoc146 <shivam97rules@gmail.com>
2018-07-18 17:17:23 +0530

    added tabulation implemntation

--- DP_tiling_problem.cpp
+++ DP_tiling_problem.cpp
@@ -3,7 +3,9 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-int func(int n){
+
+/*
+int func_1(int n){
 	if(n==2)
 		return 2;
 	if(n==1)
@@ -13,9 +15,24 @@ int func(int n){
 
 	return func(n-1)+func(n-2);
 }
+*/
+
+//DP implementation down
+int func_2(int n){
+	int memo[n+1];
+	
+	memo[0]=0;
+	memo[1]=1;
+	memo[2]=2;
+	for(int i=3;i<=n;i++)
+		memo[i]=memo[i-1]+memo[i-2];
+		
+	return memo[n];
+
+}
 int main(){
 	ios_base::sync_with_stdio(0);
 	int n;
 	cin>>n;
-	cout<<func(n);
+	cout<<func_2(n);
 }
