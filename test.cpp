228a928fa68ed8fe92b5554c9cf08700d64bd294
apoc146 <shivam97rules@gmail.com>
2018-08-18 11:40:16 +0530

    empty class of size 1 instead of 0

--- test.cpp
+++ test.cpp
@@ -1,38 +1,15 @@
 #include<iostream>
-#include<vector>
+#include<string>
 #include<algorithm>
-#include<cmath>
-using namespace std;
-
-int solve(vector<int> &A) {
-    long long mul;
-    for(int i=0;i<A.size();i++)
-        mul*=A[i];
-    
-    long long n=mul;
-    long long n2=n;
-    vector<long long> vec;
-    long long i;
-    
-    for(i=2;i<=sqrt(n2) && n!=1;i++){
-        while(n%i==0){
-            vec.push_back(i);
-            n=n/i;
-        }
-    }
-    
-    vector<long long>::iterator it;
-    it=unique(vec.begin(),vec.end());
-    int count=it-vec.begin();
-	cout<<   
-    return count;
-    
-}
+#include<stdlib.h>
+#include<stdio.h>
+class test{
+	int a[0];
+	//std::string s="";
+};
 
 int main(){
-	vector<int> A{1,2,3};
-	
-	int ans=solve(A);
-	cout<<"vector size"<<A.size();
+	test t;
+	std::cout<<sizeof(t);
 
 }
