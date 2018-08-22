412a93d122b9a30159ee168495d1533bf8041871
apoc146 <shivam97rules@gmail.com>
2018-08-22 12:50:18 +0530

    non empty class of size 0

---
+++ size_1_empty_class.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<stdlib.h>
+#include<stdio.h>
+class test{
+	int a[0];
+	//std::string s="";
+};
+
+int main(){
+	test t;
+	std::cout<<sizeof(t);
+
+}
